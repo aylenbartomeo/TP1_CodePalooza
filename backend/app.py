d2e611ffc7c396b0ae708a83eb910f1cbe8e3be4
aylenbartomeo <abartomeo@fi.uba.ar>
2024-06-29 23:35:02 -0300

    Creado endpoint /api/<id_dia>/artistas/

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
 from models import db, Artista, Dia, Escenario, Show, Sponsor
 
 app = Flask(__name__)
@@ -6,10 +9,33 @@ port = 5000
 #le pasas a sqlalchemy la url de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:ACN041003@localhost:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+engine = create_engine("postgresql+psycopg2://postgres:ACN041003@localhost:5432/postgres")
 
 @app.route('/')
-def hello_world():
-  return '¡Hola, Mundo!'
+def home():
+  dias = Dia.query.all()
+  return render_template('index.html', dias=dias)
+
+app = Flask(__name__)
+
+@app.route('/api/<id_dia>/artistas/')
+def obtener_artistas():
+	conn = engine.connect()
+	try:
+		# Consulta para obtener los artistas por id_dia
+		query = text("Select a.nombre, a.imagen, d.fecha from artistas a, dias d, shows s where s.id_artista = a.id_artista and s.id_dia = d.id_dia;")
+		result = conn.execute(query)
+		artistas = result.fetchall()
+		lista_artistas = []
+		for artista in artistas:
+			artista_data = {
+				'nombre': artista.nombre,
+				'imagen': artista.imagen  # nos falta
+			}
+			lista_artistas.append(artista_data)
+		return jsonify(lista_artistas), 200
+	except:
+		return jsonify({"mensaje": "No hay artistas."}), 500
 
 if __name__ == '__main__':
 	print('Starting server...')
